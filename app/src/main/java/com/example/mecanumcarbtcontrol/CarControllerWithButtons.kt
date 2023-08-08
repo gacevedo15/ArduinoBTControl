5abf5546dcca4a471a7681dd78b68c8c2e314bc5
gacevedo15 <gacevedo15@alumno.uned.es>
2023-08-08 13:58:34 +0200

    Nuevos vectors para los botones de controlar el coche arduino y cambiamos el icon de la app

--- app/src/main/java/com/example/mecanumcarbtcontrol/CarControllerWithButtons.kt
+++ app/src/main/java/com/example/mecanumcarbtcontrol/CarControllerWithButtons.kt
@@ -9,7 +9,7 @@ import com.airbnb.lottie.LottieAnimationView
 class CarControllerWithButtons : ComponentActivity() {
 
     //Variables para controlar el estado del switch
-    private var switchONOFF = true
+    private var switchONOFF = false
     private lateinit var switchAnimationView: LottieAnimationView
     private var animationRes = if (switchONOFF) R.raw.switchoff else R.raw.switchon
 
@@ -31,23 +31,22 @@ class CarControllerWithButtons : ComponentActivity() {
     }
 
     private fun setupSwitchAnimation() {
-        //Controlar la animacion con clickListener
+        //Controlamos la animación con setOnClickListener
         switchAnimationView.setOnClickListener {
-            // Cambiar el valor del switch y actualizar la animación correspondiente
+            // Cambiamos el valor del switch y actualizamos la animación correspondiente
             switchONOFF = !switchONOFF
-            animationRes = if (switchONOFF) R.raw.switchoff else R.raw.switchon
+            animationRes = if (switchONOFF) R.raw.switchon else R.raw.switchoff
             switchAnimationView.setAnimation(animationRes)
             switchAnimationView.playAnimation()
             //Toast que muestre "encendido" o "apagado" según el valor del switch
-            val toastText = if (!switchONOFF) "Encendido" else "Apagado"
+            val toastText = if (switchONOFF) "Encendido" else "Apagado"
             Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show()
+            myBluetooth.sendCommand(toastText)
         }
     }
-
-
-
     override fun onDestroy() {
         super.onDestroy()
+        myBluetooth.closeConnection()
     }
 
     fun onBack(view: View) {
@@ -56,57 +55,67 @@ class CarControllerWithButtons : ComponentActivity() {
 
     fun onRotateClockwise(view: View) {
         Toast.makeText(this, "Rotate Clockwise", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Rotate Clockwise")
     }
 
     fun onDriftLeft(view: View) {
         Toast.makeText(this, "Drift Left", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Drift Left")
     }
 
     fun onDriftRight(view: View) {
         Toast.makeText(this, "Drift Right", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Drift Right")
     }
 
     fun onRotateCounterClockwise(view: View) {
         Toast.makeText(this, "Rotate Counter Clockwise", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Rotate Counter Clockwise")
     }
 
     fun onUpLeft(view: View) {
         Toast.makeText(this, "Up Left", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Up Left")
     }
 
     fun onUp(view: View) {
         Toast.makeText(this, "Up", Toast.LENGTH_SHORT).show()
-        myBluetooth.sendCommand("w")
-        //Toast que muestre el comando enviado
-        Toast.makeText(this, "w enviado", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Up")
     }
 
     fun onUpRight(view: View) {
         Toast.makeText(this, "Up Right", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Up Right")
     }
 
     fun onLeft(view: View) {
         Toast.makeText(this, "Left", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Left")
     }
 
     fun onAuto(view: View) {
         Toast.makeText(this, "Auto", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Auto")
     }
 
     fun onRight(view: View) {
         Toast.makeText(this, "Right", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Right")
     }
 
     fun onDownLeft(view: View) {
         Toast.makeText(this, "Down Left", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Down Left")
     }
 
     fun onDown(view: View) {
         Toast.makeText(this, "Down", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Down")
     }
 
     fun onDownRight(view: View) {
         Toast.makeText(this, "Down Right", Toast.LENGTH_SHORT).show()
+        myBluetooth.sendCommand("Down Right")
     }
 
 
